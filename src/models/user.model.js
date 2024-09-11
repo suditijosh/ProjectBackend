af5ad32c8c9c18e68cfdc6f73534494785365fc1
suditi <sjjpr5@gmail.com>
2024-09-11 12:47:12 +0530

    add user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,8 @@
 import mongoose, {Schema} from 'mongoose'
-import jwt from jsonwebtoken
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+
+
 
 
 
@@ -74,6 +76,31 @@ userSchema.pre("save",async function (next){
          return await  bcrypt.compare(password, this.password)
     }
 
+    userSchema.methods.generateAccessToken = function () {
+        return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+         }
+    )
+    } 
+    userSchema.methods.generateRefreshToken = function() {
+        return jwt.sign(
+            {
+                _id: this._id,
+            },
+        process.env.REFERESH_TOKEN_SECRET,
+            {
+                 expiresIn: process.env.REFERESH_TOKEN_EXPIRY
+             }
+        )
+    }
 
 
 
